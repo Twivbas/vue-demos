32247acf5cc2fb698b1781b0b4f6d9e5eba6f229
Twivbas <1423228908@qq.com>
2021-06-07 14:46:11 +0800

    add demo11

---
+++ demo11-vue-router练习/src/views/Message.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(message) of messages" :key="message.id">
+        <router-link :to="`/home/message/detail/${message.id}`" >{{ message.title }}</router-link>
+        <button @click="pushShow(message.id)">push查看</button>
+        <button @click="replaceShow(message.id)">replace查看</button>
+      </li>
+    </ul>
+    <button @click="$router.back()">回退</button>
+    <hr>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      messages: []
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.messages = [
+        {
+          id: 1,
+          title: 'message001'
+        },
+        {
+          id: 2,
+          title: 'message002'
+        },
+        {
+          id: 4,
+          title: 'message004'
+        }
+      ]
+    }, 1000)
+  },
+  methods: {
+    pushShow (id) {
+      this.$router.push(`/home/message/detail/${id}`)
+    },
+    replaceShow (id) {
+      this.$router.replace(`/home/message/detail/${id}`)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ demo11-vue-router练习/src/views/MessageDetail.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <p>ID: {{ $route.params.id }}</p>
+    <ul>
+      <li>id: {{ messageDetail.id }}</li>
+      <li>title: {{ messageDetail.title }}</li>
+      <li>content: {{ messageDetail.content }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      messageDetail: {}
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      const allMessageDetails = [
+        {
+          id: 1,
+          title: 'message001',
+          content: 'content1'
+        },
+        {
+          id: 2,
+          title: 'message002',
+          content: 'content2'
+        },
+        {
+          id: 4,
+          title: 'message004',
+          content: 'content4'
+        }
+      ]
+
+      this.allMessageDetails = allMessageDetails
+      const id = +this.$route.params.id
+      this.messageDetail = allMessageDetails.find(detail => detail.id === id)
+    }, 1000)
+  },
+  watch: {
+    $route: function (value) {
+      // 路由路径（params)发生了改变
+      const id = +value.params.id
+      this.messageDetail = this.allMessageDetails.find(detail => detail.id === id)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
